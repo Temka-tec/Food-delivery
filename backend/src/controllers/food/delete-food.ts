6a0efb006ec5ee1cbe9efec07e1813245df01bd7
Temka-Tec <temkatekoo198@gmail.com>
2026-06-13 11:41:53 +0800

    deploy

--- backend/src/controllers/food/delete-food.ts
+++ backend/src/controllers/food/delete-food.ts
@@ -2,7 +2,8 @@ import type { RequestHandler } from "express";
 import { prisma } from "../../database/index.ts";
 
 export const deleteFood: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id as string;
+  await prisma.orderItem.deleteMany({ where: { foodId: id } });
   await prisma.food.delete({ where: { id } });
   res.status(200).json({ message: "Deleted" });
 };
